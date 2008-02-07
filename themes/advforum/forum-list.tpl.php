83e9fd643920879b0e265020e5ee333983b65cef
Michelle <git@shellmultimedia.com>
2008-02-07 20:38:06 +0000

    #207140 (comment 4) Added last post link to forum topic list. Thanks, LasseP.

--- themes/advforum/forum-list.tpl.php
+++ themes/advforum/forum-list.tpl.php
@@ -39,7 +39,6 @@
             array('data' => t('Last post'),'class' => 'f-last-reply')
             );
         }*/
-
       }
       else {
         if ($user->uid) {
@@ -65,11 +64,24 @@
         }
         $description .= "</div>\n";
 
+        // LAST POST
+        $fLastPost = $forum->last_post;
+        $lastPostTitle = advanced_forum_get_last_topic($fLastPost->uid, $forum->tid);
+        if ($fLastPost && $fLastPost->timestamp) {
+            $lpURL = $lastPostTitle->nid;
+            if($lastPostTitle->resType == "comment") {
+             $lpURL .= '#comment-'. $lastPostTitle->cid;
+            }
+          $last_post =  t('<a href="node/'. $lpURL .'">@title</a><br />@time ago<br />by !author', array('@title' => $lastPostTitle->nodeTitle, '@time' => format_interval(time() - $fLastPost->timestamp), '!author' => theme('username', $fLastPost)));
+        } else {
+          $last_post =  t('n/a');
+        }
+
         $rows[] = array(
           array('data' => $description, 'class' => 'forum'),
           array('data' => $forum->num_topics . ($new_topics ? '<br />'. l(format_plural($new_topics, '1 new', '@count new'), "forum/$forum->tid", NULL, NULL, 'new') : ''), 'class' => 'topics'),
           array('data' => $forum->num_posts, 'class' => 'posts'),
-          array('data' => _forum_format($forum->last_post), 'class' => 'last-reply'));
+          array('data' => $last_post, 'class' => 'last-reply'));
       }
     }
 
