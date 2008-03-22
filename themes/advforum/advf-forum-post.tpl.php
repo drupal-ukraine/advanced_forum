e5de0de3c281c81e563a1f49c5725fec425d058e
Michelle <git@shellmultimedia.com>
2008-03-22 22:16:40 +0000

    #237362 Fixed variable listing comments on advf-forum-post.tpl.php.

--- themes/advforum/advf-forum-post.tpl.php
+++ themes/advforum/advf-forum-post.tpl.php
@@ -6,23 +6,31 @@
  * Changes here will affect an individual forum post.
  
  * The following standard variables are available to you:
-    $top_post    - TRUE if we are formatting the main post (ie, not a comment)
-    $title       - Title of this post/comment
-    $content     - Content of this post/comment
-    $reply_link  - Separated out link to reply to topic
-    $links       - Formatted links (reply, edit, delete, etc)
-    $links_array - Unformatted array of links
-    $submitted   - Formatted date post/comment submitted
+    $is_forum       - TRUE if this post is in the forum. Will always be true unless
+                    - the function was called via custom code.
+    $node/$comment  - The complete node/comment object depending on value of $top_post
+    $top_post       - TRUE if we are formatting the main post (ie, not a comment)
+    $title          - Title of this post/comment
+    $content        - Content of this post/comment
+    $reply_link     - Separated out link to reply to topic
+    $links          - Formatted links (reply, edit, delete, etc)
+    $links_array    - Unformatted array of links
+    $submitted      - Formatted date post/comment submitted
+    $comment_link   - If on a comment, this is the linked # of it 
+    $page_link      - Link created by commentpage module, if installed
 
-    $accountid   - ID of the poster
-    $name        - User name of poster
+    $accountid      - ID of the poster
+    $user_info_pane - Full user info pane from theme_forum_user. 
+                      See advf-forum-user.tpl.php for variable list.
  */
 ?>
 
 <?php
-// If this is the top post (that is, the node) give it an extra wrapper to allow for special theming
 if ($top_post) {
+  // If this is the top post (that is, the node) give it an extra wrapper to allow for special theming
   $postclass = "top-post";
+  
+  // Print the big "reply" button at the top of the thread.
   print $reply_link;
 }
 ?>
