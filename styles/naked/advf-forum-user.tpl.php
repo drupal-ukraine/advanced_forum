adf4d4ca553124cebd0c684d0466621722da46c1
Michelle <git@shellmultimedia.com>
2008-09-27 04:36:58 +0000

    #303549 by Michelle: Cleaned up author info pane.

--- styles/naked/advf-forum-user.tpl.php
+++ styles/naked/advf-forum-user.tpl.php
@@ -8,20 +8,17 @@
  * Available variables (core modules):
  * - $account: The entire user object.
  * - $picture: Themed user picture.
- * - $name: Themed user name wrapped in a div class "username". 
- * - $name_raw: Themed user name. 
+ * - $account_name: Themed user name. 
+ * - $account_id: User ID number. 
  *
- * - $joined: Date the user joined the site in the format "Joined: DATE"
- * - $joined_raw: Date the user joined the site with no markup
- * - $joined_ago: Time since the user registered in the format "Joined: TIME ago"
- * - $joined_ago_raw: Amount of time since the user joined the site with no markup.
+ * - $joined: Date the user joined the site.
+ * - $joined_ago: Time since the user registered in the format "TIME ago"
  *
  * - $online_icon: Online/Offline icon.
  * - $online_status: Translated text "Online" or "Offline"
  *
- * - $contact: Marked up and linked icon and text.
- * - $contact_icon: Unlinked icon.
- * - $contact_link: raw link to user contact page. ie: 'user/1/contact'.
+ * - $contact: Linked icon.
+ * - $contact_link: Linked translated text "Contact user".
  * 
  * - $profile - Profile object from core profile. 
  *     Usage: $profile['category']['field_name']['value']
@@ -30,51 +27,73 @@
  * Available variables (contributed modules):
  * - $facebook_status: Status from the facebook status module.
  *
- * - $privatemsg: Marked up and linked icon and text.  
- * - $privatemsg_icon: Unlinked icon. 
- * - $privatemsg_link: Raw link to send private message to user. 
+ * - $privatemsg: Linked icon.  
+ * - $privatemsg_link: Linked translated text "Send PM".
  *
- * - $user_stats_posts: Marked up number of posts from user stats module.
- * - $user_stats_posts_raw: Just the post count.
- * - $user_stats_ip: Marked up IP address from user stats module.
- * - $user_stats_ip_raw: Just the IP address.
+ * - $user_stats_posts: Number of posts from user stats module.
+ * - $user_stats_ip: IP address from user stats module.
  *
- * - $userpoints_points: Marked up number of points from userpoints module.
- * - $userpoints_points_raw: Just the points count.
+ * - $userpoints_points: Number of points from userpoints module.
  *
  * 5.x only at this time:
  *
- * - $buddylist: Marked up and linked icon and text.  
- * - $buddylist_icon: Unlinked icon. 
- * - $buddylist_link: Raw link to send add/remove poster as a buddy. 
+ * - $buddylist: Linked icon.  
+ * - $buddylist_link: Linked translated text "Add to buddylist" or "Remove from buddylist". 
  *
  * - $subscribe: Formatted link to subscribe to the author's forum topics.
  * - $subscribe_link: As above but just the relative path.
  *
- * - $user_title: Marked up title from user titles module.
- * - $user_title_raw: Just the title.
+ * - $user_title: Title from user titles module.
  *
- * - $user_badges: Marked up badges from user badges module.
- * - $user_badges_raw: Just the badges.
-
+ * - $user_badges: Badges from user badges module.
+ 
 */
 ?>
-<div class="user-info-inner">
+<div class="author-info-inner">
 
-  <?php print $name; ?>
-  <?php if (isset($user_title)) print $user_title; ?>
-  <?php if (isset($user_badges)) print $user_badges;  ?>
-  <?php print $picture; ?>
+  <div class="author-info-line author-name"> <?php print $account_name; ?> </div>
+
+  <?php if (!empty($user_title)): ?>
+    <div class="author-info-line author-title"> <?php print $user_title; ?> </div>
+  <?php endif; ?>
+  
+  <?php if (!empty($user_badges)): ?>
+    <div class="author-info-line author-badges"> <?php print $user_badges;  ?> </div>
+  <?php endif; ?>
+
+  <?php if (!empty($picture)): ?>
+    <?php print $picture; ?>
+  <?php endif; ?>
+  
+  <div class="author-info-line author-id"><span class="author-info-label">Member:</span> <?php print $account_id; ?> </div>
+  
+  <div class="author-info-line author-joined"><span class="author-info-label">Joined:</span> <?php print $joined; ?> </div>
+  
+  <?php if (isset($user_stats_posts)): ?>
+    <div class="author-info-line author-posts"><span class="author-info-label">Posts:</span> <?php print $user_stats_posts; ?> </div>
+  <?php endif; ?>
+  
+  <?php if (isset($userpoints_points)): ?>
+    <div class="author-info-line author-points"><span class="author-info-label">Points:</span> <?php print $userpoints_points; ?> </div>
+  <?php endif; ?>
+  
+  <?php if (!empty($user_stats_ip)): ?>
+    <div class="author-info-line author-ip"><span class="author-info-label">IP:</span> <?php print $user_stats_ip; ?> </div>
+  <?php endif; ?>
+  
   <br />
-  <?php print $joined; ?>
-  <?php if (isset($user_stats_posts)) print $user_stats_posts; ?>
-  <?php if (isset($userpoints_points)) print $userpoints_points; ?>
-  <br />
-  <?php if (isset($contact)) print $contact; ?>
-  <?php if (isset($privatemsg)) print $privatemsg; ?>
-  <?php if (isset($buddylist)) print $buddylist; ?>
-  <br />
-  <?php print $online_status; ?>
-  <?php if (isset($users_stats_ip)) print $user_stats_ip; ?>
+  <div class="author-info-icon"><?php print $online_icon; ?></div>
+
+  <?php if (!empty($contact)): ?>
+    <div class="author-info-icon"><?php print $contact; ?></div>
+  <?php endif; ?>
+  
+  <?php if (!empty($privatemsg)): ?>
+    <div class="author-info-icon"><?php print $privatemsg; ?></div>
+  <?php endif; ?>
+  
+  <?php if (!empty($buddylist)): ?>
+    <div class="author-info-icon"><?php print $buddylist; ?></div>
+  <?php endif; ?>
   
 </div>
