0a2dca25e389d500829adea18a23c0b52d2521fc
Michelle <git@shellmultimedia.com>
2008-10-21 21:11:31 +0000

    NOISSUE by Michelle: Added blue_lagoon_stacked style.

---
+++ styles/blue_lagoon_stacked/advf-author-pane.tpl.php
@@ -0,0 +1,116 @@
+<?php
+// $Id$
+
+/**
+ * @file advf-forum-user.tpl.php
+ * Theme implementation to display information about the posting user.
+ *
+ * Available variables (core modules):
+ * - $account: The entire user object.
+ * - $picture: Themed user picture.
+ * - $account_name: Themed user name.
+ * - $account_id: User ID number.
+ *
+ * - $joined: Date the user joined the site.
+ * - $joined_ago: Time since the user registered in the format "TIME ago"
+ *
+ * - $online_icon: Online/Offline icon.
+ * - $online_status: Translated text "Online" or "Offline"
+ *
+ * - $contact: Linked icon.
+ * - $contact_link: Linked translated text "Contact user".
+ *
+ * - $profile - Profile object from core profile.
+ *     Usage: $profile['category']['field_name']['value']
+ *     Example: <?php print $profile['Personal info']['profile_name']['value']; ?>
+ *
+ * Available variables (contributed modules):
+ * - $facebook_status: Status from the facebook status module.
+ *
+ * - $privatemsg: Linked icon.
+ * - $privatemsg_link: Linked translated text "Send PM".
+ *
+ * - $user_stats_posts: Number of posts from user stats module.
+ * - $user_stats_ip: IP address from user stats module.
+ *
+ * - $userpoints_points: Number of points from default category of the userpoints module.
+ * - $userpoints_categories: Array holding each category and the points for that category.
+ *
+ * 5.x only at this time:
+ *
+ * - $buddylist: Linked icon.
+ * - $buddylist_link: Linked translated text "Add to buddylist" or "Remove from buddylist".
+ *
+ * - $subscribe: Formatted link to subscribe to the author's forum topics.
+ * - $subscribe_link: As above but just the relative path.
+ *
+ * - $user_title: Title from user titles module.
+ *
+ * - $user_badges: Badges from user badges module.
+
+*/
+?>
+<div class="author-pane-inner">
+
+  <div class="author-pane-first">
+
+    <?php if (!empty($picture)): ?>
+      <?php print $picture; ?>
+    <?php endif; ?>
+
+    <div class="author-pane-name-section">
+      <div class="author-pane-line author-name"> <?php print $account_name; ?> </div>
+
+      <?php if (!empty($user_title)): ?>
+        <div class="author-pane-line author-title"> <?php print $user_title; ?> </div>
+      <?php endif; ?>
+
+      <?php if (!empty($user_badges)): ?>
+        <div class="author-pane-line author-badges"> <?php print $user_badges;  ?> </div>
+      <?php endif; ?>
+    </div>
+  </div>
+
+  <div class="author-pane-last">
+
+    <?php if (!empty($joined)): ?> 
+      <div class="author-pane-line author-joined">
+        <span class="author-pane-label"><?php print t('Joined'); ?>:</span> <?php print $joined; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (isset($user_stats_posts)): ?>
+      <div class="author-pane-line author-posts">
+        <span class="author-pane-label"><?php print t('Posts'); ?>:</span> <?php print $user_stats_posts; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (isset($userpoints_points)): ?>
+      <div class="author-pane-line author-points">
+        <span class="author-pane-label"><?php print t('!Points: ', userpoints_translation()); ?></span> <?php print $userpoints_points; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($user_stats_ip)): ?>
+      <div class="author-pane-line author-ip">
+        <span class="author-pane-label"><?php print t('IP'); ?>:</span> <?php print $user_stats_ip; ?>
+      </div>
+    <?php endif; ?>
+
+    <div class="author-pane-icon"><?php print $online_icon; ?></div>
+
+    <?php if (!empty($contact)): ?>
+      <div class="author-pane-icon"><?php print $contact; ?></div>
+    <?php endif; ?>
+
+    <?php if (!empty($privatemsg)): ?>
+      <div class="author-pane-icon"><?php print $privatemsg; ?></div>
+    <?php endif; ?>
+
+    <?php if (!empty($buddylist)): ?>
+      <div class="author-pane-icon"><?php print $buddylist; ?></div>
+    <?php endif; ?>
+
+  </div>
+
+</div>
---
+++ styles/blue_lagoon_stacked/advf-forum-post.tpl.php
@@ -0,0 +1,97 @@
+<?php
+// $Id$
+
+/**
+ * advf-forum-post.tpl.php is the template file for both
+ * the top post (the node) and the comments/replies
+ * Changes here will affect an individual forum post.
+
+ * The following standard variables are available to you:
+ * - $top_post: TRUE if we are formatting the main post (ie, not a comment)
+ * - $title: Title of this post/comment
+ * - $content: Content of this post/comment
+ * - $reply_link: Separated out link to reply to topic
+ * - $jump_first_new: Shows number of new (to user) comments and links to the first one
+ * - $links: Formatted links (reply, edit, delete, etc)
+ * - $links_array: Unformatted array of links
+ * - $submitted: Formatted date post/comment submitted
+
+ * - $account: User object of the poster
+ * - $name: User name of poster
+ * - $user_info_pane: Entire contents of advf-user-info.tpl.php
+
+ */
+?>
+
+<?php if ($top_post): ?>
+  <?php $postclass = "top-post"; ?>
+
+  <div class="forum-post-header clear-block">
+    <?php print $reply_link; ?>
+    
+    <div class="reply-count">
+      <?php print $total_posts; ?>
+      
+      <?php if (!empty($new_posts)): ?>
+        (<?php print $new_posts; ?>)
+      <?php endif; ?>
+    </div>
+    
+  </div>
+
+<?php endif; ?>
+
+<div class="<?php print (isset($postclass)) ? $postclass . ' ' : ''; ?>forum-comment<?php print (isset($row_class)) ? ' forum-comment-' . $row_class : ''; print (!empty($comment->new)) ? ' comment-new forum-comment-new' : ''; ?> clear-block">
+
+  <div class="post-info clear-block">
+    <div class="posted-on"><?php print $date ?>
+
+    <?php if (!$top_post): ?>
+      <?php if (!empty($comment->new)) : ?>
+         <a id="new"></a>
+        <span class="new">(<?php print $new ?>)</span>
+      <?php endif ?>
+    <?php endif; ?>
+
+    </div>
+
+    <?php if (!$top_post): ?>
+      <span class="post-num"><?php print $comment_link . ' ' . $page_link; ?></span>
+    <?php endif; ?>
+  </div>
+
+  <div class="forum-post-wrapper">
+
+    <div class="forum-comment-left">
+      <div class="author-pane">
+        <?php print $author_pane; ?>
+     </div>
+    </div>
+
+    <div class="forum-comment-right clear-block">
+      <?php if ($title && !$top_post): ?>
+        <div class="post-title">
+          <?php print $title ?>
+        </div>
+      <?php endif; ?>
+
+      <div class="content">
+        <?php print $content ?>
+      </div>
+
+    </div>
+
+    <?php if ($signature): ?>
+      <div class="author-signature clear-block">
+        <?php print $signature ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($links): ?>
+      <div class="links">
+        <?php print $links ?>
+      </div>
+    <?php endif; ?>
+
+  </div>
+</div>
