ca9ee2e64b03b1fd98571b6e2d80d4cdfd3cc4c0
Michelle <git@shellmultimedia.com>
2008-11-12 03:50:56 +0000

    NOISSUE by Michelle: Backport of style changes from D6.

--- styles/naked/advf-forum-post.tpl.php
+++ styles/naked/advf-forum-post.tpl.php
@@ -2,30 +2,27 @@
 // $Id$
 
 /**
- * advf-forum-post.tpl.php is the template file for both
- * the top post (the node) and the comments/replies
- * Changes here will affect an individual forum post.
-
- * The following standard variables are available to you:
+ * @file advf-forum-post.tpl.php
+ *
+ * Theme implementation: Template for each forum post whether node or comment.
+ *
+ * All variables available in node.tpl.php and comment.tpl.php for your theme
+ * are available here. In addition, Advanced Forum makes available the following
+ * variables: 
+ * 
  * - $top_post: TRUE if we are formatting the main post (ie, not a comment)
- * - $title: Title of this post/comment
- * - $content: Content of this post/comment
- * - $reply_link: Separated out link to reply to topic
- * - $jump_first_new: Shows number of new (to user) comments and links to the first one
- * - $links: Formatted links (reply, edit, delete, etc)
- * - $links_array: Unformatted array of links
- * - $submitted: Formatted date post/comment submitted
-
- * - $account: User object of the poster
- * - $name: User name of poster
- * - $user_info_pane: Entire contents of advf-user-info.tpl.php
+ * - $reply_link: Text link / button to reply to topic. 
+ * - %total_posts: Number of posts in topic (not counting first post).
+ * - $new_posts: Number of new posts in topic, and link to first new.
+ * - $links_array: Unformatted array of links.
+ * - $account: User object of the post author.
+ * - $name: User name of post author.
+ * - $author_pane: Entire contents of advf-author-pane.tpl.php.
 
  */
 ?>
 
 <?php if ($top_post): ?>
-  <?php $postclass = "top-post"; ?>
-
   <div class="forum-post-header clear-block">
     <?php print $reply_link; ?>
     
@@ -35,24 +32,25 @@
       <?php if (!empty($new_posts)): ?>
         (<?php print $new_posts; ?>)
       <?php endif; ?>
-    </div>
-    
+    </div>   
   </div>
 
+  <?php $classes .= $node_classes; ?>
+  <div id="node-<?php print $node->nid; ?>" class="top-post forum-post <?php print $classes; ?> clear-block">
+  <a id="top"></a>
+  
+<?php else: ?>
+  <?php $classes .= $comment_classes; ?>
+  <div id="comment-<?php print $comment->cid; ?>" class="forum-post <?php print $classes; ?> clear-block">
 <?php endif; ?>
 
-<div class="<?php print (isset($postclass)) ? $postclass . ' ' : ''; ?>forum-comment<?php print (isset($row_class)) ? ' forum-comment-' . $row_class : ''; print (!empty($comment->new)) ? ' comment-new forum-comment-new' : ''; ?> clear-block">
-
   <div class="post-info clear-block">
-    <div class="posted-on"><?php print $date ?>
+    <div class="posted-on">
+      <?php print $date ?>
 
-    <?php if (!$top_post): ?>
-      <?php if (!empty($comment->new)) : ?>
-         <a id="new"></a>
-        <span class="new">(<?php print $new ?>)</span>
-      <?php endif ?>
-    <?php endif; ?>
-
+      <?php if (!$top_post && !empty($comment->new)): ?>
+        <a id="new"><span class="new">(<?php print $new ?>)</span></a>      
+      <?php endif; ?>
     </div>
 
     <?php if (!$top_post): ?>
@@ -62,13 +60,13 @@
 
   <div class="forum-post-wrapper">
 
-    <div class="forum-comment-left">
+    <div class="forum-post-panel-sub">
       <div class="author-pane">
         <?php print $author_pane; ?>
      </div>
     </div>
 
-    <div class="forum-comment-right clear-block">
+    <div class="forum-post-panel-main clear-block">
       <?php if ($title && !$top_post): ?>
         <div class="post-title">
           <?php print $title ?>
@@ -79,19 +77,23 @@
         <?php print $content ?>
       </div>
 
+      <?php if ($signature): ?>
+        <div class="author-signature">
+          <?php print $signature ?>
+        </div>
+      <?php endif; ?>
     </div>
+  </div>
 
-    <?php if ($signature): ?>
-      <div class="author-signature clear-block">
-        <?php print $signature ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($links): ?>
-      <div class="links">
+  <div class="forum-post-footer clear-block">
+    <div class="forum-jump-links">
+      <a href="#top">Top</a>
+    </div>
+    
+    <?php if (!empty($links)): ?>
+      <div class="forum-post-links">
         <?php print $links ?>
       </div>
     <?php endif; ?>
-
   </div>
 </div>
--- styles/naked/advf-forum-repeat-post.tpl.php
+++ styles/naked/advf-forum-repeat-post.tpl.php
@@ -15,7 +15,7 @@
  */
 ?>
 
-<div class="forum-post-header clearfix">
+<div class="forum-post-header clear-block">
   <?php print $reply_link; ?>
   
   <div class="reply-count">
