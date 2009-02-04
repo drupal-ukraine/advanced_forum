b38fc377f75b68475ccb93a5d5519e70c1805032
Michelle <git@shellmultimedia.com>
2009-02-04 17:22:08 +0000

    #367481 by Michelle: Fixed up statistics including changing to use theme_username.

--- styles/boxy/advf-forum-statistics.tpl.php
+++ styles/boxy/advf-forum-statistics.tpl.php
@@ -14,13 +14,12 @@
  * - $topics: Total number of nodes (threads / topics).
  * - $posts: Total number of nodes + comments.
  * - $users: Total number of registered active users.
- * - $latest_user_name: Name of latest active user.
- * - $latest_user_id: ID of  of latest active user.
+ * - $latest_user: Linked user name of latest active user.
  */
 ?>
 
 <div id="forum-statistics">
-  <div id="forum-statistics-header"><?php print t('What\'s Going On?'); ?></div>
+  <div id="forum-statistics-header"><?php print t("What's Going On?"); ?></div>
 
   <div id="forum-statistics-active-header" class="forum-statistics-sub-header">
     <?php print t('Currently active users: !current_total (!current_users users and !current_guests guests)', array('!current_total' => $current_total, '!current_users' => $current_users, '!current_guests' => $current_guests)); ?>
@@ -36,6 +35,6 @@
 
   <div id="forum-statistics-statistics-body" class="forum-statistics-sub-body">
     <?php print t('Topics: !topics, Posts: !posts, Users: !users', array('!topics' => $topics, '!posts' => $posts, '!users' => $users)); ?>
-    <br /><?php print t('Welcome to our latest member, !user', array('!user' => l($latest_user_name, 'user/' . $latest_user_id))); ?>
+    <br /><?php print t('Welcome to our latest member, !user', array('!user' => $latest_user)); ?>
   </div>
 </div>
