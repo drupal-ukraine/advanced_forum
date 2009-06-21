ad0fde71be098c8b0c32d7de3f785b77d790f149
Michelle <git@shellmultimedia.com>
2009-06-21 05:13:07 +0000

    #462502 by Michelle: Seperated each container on the forum list into its own table. #219549 by Michelle: Moved subforums into comma seperated list. #312958 by Michelle: Show container description on container pages.

--- styles/naked/advanced_forum-forum-list.tpl.php
+++ styles/naked/advanced_forum-forum-list.tpl.php
@@ -32,74 +32,76 @@
  */
 ?>
 
-<table id="forum-<?php print $forum_id; ?>" class="forum-table">
+<?php $container_number = 0 ?>
+<?php foreach ($forums as $child_id => $forum): ?>
 
-  <thead class="forum-header">
-    <tr>
-      <th class="forum-icon"></th>
-      <th class="forum-name"><?php print t('Forum'); ?></th>
-      <th class="forum-num-topics"><?php print t('Topics');?></th>
-      <th class="forum-posts"><?php print t('Posts'); ?></th>
-      <th class="forum-last-post"><?php print t('Last post'); ?></th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <?php $container_adjustment = 0; ?>
-    <?php foreach ($forums as $child_id => $forum): ?>
-
-        <?php if ($forum->is_container): ?>
-          <tr id="forum-list-<?php print $child_id; ?>" class="<?php print $forum->zebra; ?> <?php print $forum->row_classes;?> container-<?php print $forum->container_id;?>" >
-            <td colspan="5" class="container">
-              <div class="forum-details">
-                <div class="name">
-                  <a href="<?php print $forum->link; ?>"><?php print $forum->name; ?></a>
-                </div>
-                <?php if ($forum->description): ?>
-                  <div class="description"><?php print $forum->description; ?></div>
-                <?php endif; ?>
-              </div>
-            </td>
-
-            <?php $container_adjustment = 1; ?>
-          </tr>
-        <?php else: ?>
-          <tr id="forum-list-<?php print $child_id; ?>" class="<?php print $forum->zebra; ?> <?php print $forum->row_classes;?> in-container-<?php print $forum->container_id;?>">
-            <?php if ($forum->depth == 0) {$container_adjustment = 0;} ?>
-
-            <td class="forum-icon"> <?php print $forum->icon ?> </td>
-
-            <td>
-              <?php print str_repeat('<div class="indent">', $forum->depth - $container_adjustment); ?>
-                <div class="forum-details">
-                  <div class="name"><a href="<?php print $forum->link; ?>"><?php print $forum->name; ?></a></div>
-                  <?php if ($forum->description): ?>
-                    <div class="description"><?php print $forum->description; ?></div>
-                  <?php endif; ?>
-                </div>
-              <?php print str_repeat('</div>', $forum->depth - $container_adjustment); ?>
-            </td>
-
-            <td class="topics">
-              <div class="num num-topics"><?php print $forum->num_topics ?>
-                <?php if ($forum->new_topics): ?>
-                  <div class="num num-new-topics"><a href="<?php print $forum->new_url; ?>"><?php print $forum->new_text; ?></a></div>
-                <?php endif; ?>
-              </div>
-            </td>
-
-            <td class="num posts">
-              <?php print $forum->num_posts ?>
-              <?php if ($forum->new_posts): ?>
-                  <br />
-                  <a href="<?php print $forum->new_url_posts; ?>"><?php print $forum->new_text_posts; ?></a>
-              <?php endif; ?>
-            </td>
-
-            <td class="last-reply"><?php print $forum->last_reply ?></td>
+  <?php if ($forum->is_container): ?>
+    <?php $container_number++ ?>
+    <?php if ($container_number > 1): ?>
+      </tbody></table>
+    <?php endif; ?>
+    
+    <table id="container-<?php print $container_number; ?>" class="forum-table">
+      <thead class="forum-header">
+        <tr>
+          <th class="forum-icon"></th>
+          <th class="forum-name"><a href="<?php print $forum->link; ?>"><?php print $forum->name; ?></a></th>
+          <th class="forum-num-topics"><?php print t('Topics');?></th>
+          <th class="forum-posts"><?php print t('Posts'); ?></th>
+          <th class="forum-last-post"><?php print t('Last post'); ?></th>
         </tr>
+      </thead>
+
+      <tbody>
+        <?php if ($forum->description): ?>
+          <tr class="container-description">
+            <td colspan="5">
+              <?php print $forum->description; ?>
+            </td>
+          </tr>
         <?php endif; ?>
-    <?php endforeach; ?>
+ 
+  <?php else: ?>
+    <?php if ($forum->depth == 1): ?>
+      <tr id="forum-<?php print $child_id; ?>" class="forum-row <?php print $forum->zebra; ?>  container-<?php print $container_number; ?>-child">   
+        <td class="forum-icon"> 
+          <?php print $forum->icon ?> 
+        </td>
+
+        <td class="forum-details">
+          <div class="name">
+            <a href="<?php print $forum->link; ?>"><?php print $forum->name; ?></a>
+          </div>
+          <?php if (!empty($forum->description)): ?>
+            <div class="description"><?php print $forum->description; ?></div>
+          <?php endif; ?>
+          
+          <?php if (!empty($forum->subforums)): ?>
+            <div class="subforums"><?php print $forum->subforums; ?></div>
+          <?php endif; ?>
+        </td>
+
+        <td class="topics">
+          <div class="num num-topics"><?php print $forum->num_topics ?>
+            <?php if ($forum->new_topics): ?>
+              <div class="num num-new-topics"><a href="<?php print $forum->new_url; ?>"><?php print $forum->new_text; ?></a></div>
+            <?php endif; ?>
+          </div>
+        </td>
+
+        <td class="num posts">
+          <?php print $forum->num_posts ?>
+          <?php if ($forum->new_posts): ?>
+              <br />
+              <a href="<?php print $forum->new_url_posts; ?>"><?php print $forum->new_text_posts; ?></a>
+          <?php endif; ?>
+        </td>
+
+        <td class="last-reply"><?php print $forum->last_reply ?></td>
+      </tr>
+    <?php endif; ?>
+  <?php endif; ?>
+<?php endforeach; ?>
   </tbody>
 </table>
 
