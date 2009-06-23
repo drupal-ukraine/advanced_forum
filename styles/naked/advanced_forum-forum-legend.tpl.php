d5dc8c38136a033549f983b02532a56929e67f56
Michelle <git@shellmultimedia.com>
2009-06-23 02:59:10 +0000

    #497474 by Michelle: Rewrite of the style system that adds a dependency on CTools as well as substyles. Adjusted all included styles to take advantage of that.

--- styles/naked/advanced_forum-forum-legend.tpl.php
+++ styles/naked/advanced_forum-forum-legend.tpl.php
@@ -7,14 +7,7 @@
  *
  */
 ?>
-<div class="forum-folder-legend forum-smalltext">
-  <dl>
-    <dt><?php print $folder_new_posts; ?></dt>
-    <dd><?php print t('Forum Contains New Posts'); ?></dd>
-    <dt><?php print $folder_default; ?></dt>
-    <dd><?php print t('Forum Contains No New Posts'); ?></dd>
-    <dt><?php print $folder_locked ?></dt>
-    <dd><?php print t('Forum is Locked'); ?></dd>
-  </dl>
+<div class="forum-folder-legend clear-block">
+  <div class="forum-folder-new-posts"><?php print t('Forum Contains New Posts'); ?></div>
+  <div class="forum-folder"><?php print t('Forum Contains No Posts'); ?></div>
 </div>
-<br style="clear: both;" />
