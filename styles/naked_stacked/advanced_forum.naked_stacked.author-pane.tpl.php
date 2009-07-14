4e7d5b2be31b9b39f00f20de7d1ea6c80a0aef18
Michelle <git@shellmultimedia.com>
2009-07-14 22:48:18 +0000

    NOISSUE by Michelle: Cleaned up Author Pane of stacked styles.

--- styles/naked_stacked/advanced_forum.naked_stacked.author-pane.tpl.php
+++ styles/naked_stacked/advanced_forum.naked_stacked.author-pane.tpl.php
@@ -10,103 +10,120 @@
 ?>
 
 <div class="author-pane">
-  <div class="author-pane-inner">
-    <div class="author-pane-top">
-      <div class="author-pane-picture author-pane-section">
-        <?php if (!empty($picture)): ?>
-          <?php print $picture; ?>
-        <?php endif; ?>
+ <div class="author-pane-inner">
+    <?php /* Avatar (has div in variable) */ ?>
+    <?php if (!empty($picture)): ?>
+      <?php print $picture; ?>
+    <?php endif; ?>
+
+    <?php /* General section */ ?>
+    <div class="author-pane-section author-pane-section-1">
+      <?php /* Account name */ ?>
+      <div class="author-pane-line author-name"> 
+        <?php print $account_name; ?> 
       </div>
 
-      <div class="author-pane-name-status author-pane-section">
-        <div class="author-pane-line author-name"> <?php print $account_name; ?> </div>
-
-        <?php if (!empty($facebook_status_status)): ?>
-          <div class="author-pane-line author-facebook-status"><?php print $facebook_status_status;  ?></div>
-        <?php endif; ?>
-
-        <div class="author-pane-line author-pane-online">
-          <span class="author-pane-online-icon"><?php print $online_icon; ?></span>
-          <span class="author-pane-online-status"><?php print $online_status; ?></span>
+      <?php /* Online status */ ?>
+      <?php if (!empty($online_status)): ?>
+        <div class="author-pane-line <?php print $online_status_class ?>">
+           <?php print $online_status; ?>
         </div>
+      <?php endif; ?>
 
-        <?php if (!empty($user_title)): ?>
-          <div class="author-pane-line author-title"> <?php print $user_title; ?> </div>
-        <?php endif; ?>
-
-        <?php if (!empty($user_badges)): ?>
-          <div class="author-pane-line author-badges"> <?php print $user_badges;  ?> </div>
-        <?php endif; ?>
-
-        <?php if (!empty($location)): ?>
-          <div class="author-pane-line author-location"> <?php print $location;  ?> </div>
-        <?php endif; ?>
-      </div>
-
-      <div class="author-pane-admin author-pane-section">
-        <?php if (!empty($user_stats_ip)): ?>
-          <div class="author-pane-line author-ip">
-            <span class="author-pane-label"><?php print t('IP'); ?>:</span> <?php print $user_stats_ip; ?>
-          </div>
-        <?php endif; ?>
-
-        <?php if (!empty($fasttoggle_block_author)): ?>
-          <div class="author-pane-line  author-fasttoggle-block"><?php print $fasttoggle_block_author; ?></div>
-        <?php endif; ?>
-
-        <?php if (!empty($troll_ban_author)): ?>
-          <div class="author-pane-line author-troll-ban"><?php print $troll_ban_author; ?></div>
-        <?php endif; ?>        
-      </div>
-
-      <div class="author-pane-stats author-pane-contact author-pane-section clear-block">
-        <?php if (!empty($joined)): ?>
-          <div class="author-pane-line author-joined">
-            <span class="author-pane-label"><?php print t('Joined'); ?>:</span> <?php print $joined; ?>
-          </div>
-        <?php endif; ?>
-
-        <?php if (isset($user_stats_posts)): ?>
-          <div class="author-pane-line author-posts">
-            <span class="author-pane-label"><?php print t('Posts'); ?>:</span> <?php print $user_stats_posts; ?>
-          </div>
-        <?php endif; ?>
-
-        <?php if (isset($userpoints_points)): ?>
-          <div class="author-pane-line author-points">
-            <span class="author-pane-label"><?php print t('!Points', userpoints_translation()); ?></span>: <?php print $userpoints_points; ?>
-          </div>
-        <?php endif; ?>
-
-        <?php if (!empty($contact)): ?>
-          <div class="author-pane-icon"><?php print $contact; ?></div>
-        <?php endif; ?>
-
-        <?php if (!empty($privatemsg)): ?>
-          <div class="author-pane-icon"><?php print $privatemsg; ?></div>
-        <?php endif; ?>
-
-        <?php if (!empty($buddylist)): ?>
-          <div class="author-pane-icon"><?php print $buddylist; ?></div>
-        <?php endif; ?>
-
-        <?php if (!empty($user_relationships_api)): ?>
-          <div class="author-pane-icon"><?php print $user_relationships_api; ?></div>
-        <?php endif; ?>
-        
-        <?php if (!empty($flag_friend)): ?>
-          <div class="author-pane-icon"><?php print $flag_friend; ?></div>
-        <?php endif; ?>
-      </div>
+      <?php /* User title */ ?>
+      <?php if (!empty($user_title)): ?>
+        <div class="author-pane-line author-title"> 
+          <?php print $user_title; ?> 
+        </div>
+      <?php endif; ?>
+      
+      <?php /* User badges */ ?>
+      <?php if (!empty($user_badges)): ?>
+        <div class="author-pane-line author-badges"> 
+          <?php print $user_badges; ?> 
+        </div>
+      <?php endif; ?>
     </div>
 
-    <div class="author-pane-groups author-pane-section">
-      <?php if (isset($og_groups)): ?>
-         <div class="author-pane-line author-groups">
-           <span class="author-pane-label"><?php print t('Groups'); ?>:</span> <?php print $og_groups; ?>
-         </div>
-       <?php endif; ?>      
+    <div class="author-pane-section author-pane-section-2">
+      <?php /* Location */ ?>
+      <?php if (!empty($location)): ?>
+        <div class="author-pane-line author-location"> 
+          <?php print $location;  ?> 
+        </div>
+      <?php endif; ?>
+      
+      <?php /* Joined */ ?>
+      <?php if (!empty($joined)): ?>
+        <div class="author-pane-line author-joined">
+          <span class="author-pane-label"><?php print t('Joined'); ?>:</span> <?php print $joined; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php /* Posts */ ?>
+      <?php if (isset($user_stats_posts)): ?>
+        <div class="author-pane-line author-posts">
+          <span class="author-pane-label"><?php print t('Posts'); ?>:</span> <?php print $user_stats_posts; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php /* Points */ ?>
+      <?php if (isset($userpoints_points)): ?>
+        <div class="author-pane-line author-points">
+          <span class="author-pane-label"><?php print t('!Points', userpoints_translation()); ?></span>: <?php print $userpoints_points; ?>
+        </div>
+      <?php endif; ?>
     </div>
+
+    <?php /* Contact section */ ?>
+    <div class="author-pane-section author-pane-contact">
+      <?php /* Contact / Email */ ?>
+      <?php if (!empty($contact_link)): ?>
+        <div class="author-pane-line author-contact">
+          <?php print $contact_link; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php /* Private message */ ?>
+      <?php if (!empty($privatemsg)): ?>
+        <div class="author-pane-line author-privatemsg">
+          <?php print $privatemsg_link; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php /* User relationships */ ?>
+      <?php if (!empty($user_relationships_api_link)): ?>
+        <div class="author-pane-line author-user-relationship">
+          <?php print $user_relationships_api_link; ?>
+        </div>
+      <?php endif; ?>
+      
+      <?php /* Flag friend */ ?>
+      <?php if (!empty($flag_friend)): ?>
+        <div class="author-pane-line">
+          <?php print $flag_friend; ?>
+        </div>
+      <?php endif; ?>
+    </div>
+
+    <?php /* Admin section */ ?>
+    <div class="author-pane-section author-pane-admin">
+      <?php /* IP */ ?>
+      <?php if (!empty($user_stats_ip)): ?>
+        <div class="author-pane-line author-ip">
+          <span class="author-pane-label"><?php print t('IP'); ?>:</span> <?php print $user_stats_ip; ?>
+        </div>
+      <?php endif; ?>
+
+     <?php /* Fasttoggle block */ ?>
+     <?php if (!empty($fasttoggle_block_author)): ?>
+        <div class="author-fasttoggle-block"><?php print $fasttoggle_block_author; ?></div>
+      <?php endif; ?>
+
+     <?php /* Troll ban */ ?>
+      <?php if (!empty($troll_ban_author)): ?>
+        <div class="author-pane-line author-troll-ban"><?php print $troll_ban_author; ?></div>
+      <?php endif; ?>        
+    </div>    
   </div>
 </div>
-
