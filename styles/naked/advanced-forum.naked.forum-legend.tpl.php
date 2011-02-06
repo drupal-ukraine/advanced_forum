16783f1b2754e9c50831e9e411cab67282ee638b
troky <troky@1013060.no-reply.drupal.org>
2011-02-06 12:14:14 +0000

    #868942 by Michelle: Changed forum-folder and forum-icon to forum-list-icon to reflect the fact that the icon is not necessarily a folder. #1012238 by Michelle: Removed undead boxy and boxy_stacked that came back to life.

--- styles/naked/advanced-forum.naked.forum-legend.tpl.php
+++ styles/naked/advanced-forum.naked.forum-legend.tpl.php
@@ -8,7 +8,26 @@
  */
 ?>
 
-<div class="forum-folder-legend clearfix">
-  <div class="forum-folder-new-posts"><?php print t('Forum contains new posts'); ?></div>
-  <div class="forum-folder"><?php print t('Forum contains no new posts'); ?></div>
+<div class="forum-list-icon-legend clearfix">
+  <div class="forum-list-legend-item">
+    <span class="forum-list-icon-wrapper">
+      <span class="forum-list-icon-new-posts">
+      </span>
+    </span>
+
+    <span class="forum-list-text-legend-new-posts">
+      <?php print t('New posts'); ?>
+    </span>
+  </div>
+
+  <div class="forum-list-legend-item">
+    <span class="forum-list-icon-wrapper">
+      <span class="forum-list-icon-default">
+      </span>
+    </span>
+
+    <span class="forum-list-text-legend-default">
+      <?php print t('No new posts'); ?>
+    </span>
+  </div>
 </div>
